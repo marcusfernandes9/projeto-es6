aea2cc22899c633de916a415868156311916e8b1
-GitName mfernandes <-GitEmail mfernandes@esss.co>
2019-09-10 19:08:32 -0300

    Renderizando lista na tela

--- src/main.js
+++ src/main.js
@@ -1,1 +1,54 @@
-alert('Marcus')
+class App {
+    constructor() {
+        this.repositories = [];
+        this.formEl = document.getElementById('repo-form')
+        this.listEl = document.getElementById('repo-list')
+
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event)
+    }
+
+    addRepository(event) {
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire sua ideia do papel e de vida a sua startup',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'https://github.com/Rocketseat'
+        })
+        this.render()
+    }
+
+    render() {
+        this.listEl.innerHTML = ''
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img')
+            imgEl.setAttribute('src', repo.avatar_url)
+
+            let titleEl = document.createElement('stronf')
+            titleEl.appendChild(document.createTextNode(repo.name))
+
+            let descriptionEl = document.createElement('p')
+            descriptionEl.appendChild(document.createTextNode(repo.description))
+
+            let linkEl = document.createElement('a')
+            linkEl.appendChild(document.createTextNode('Acessar'))
+            linkEl.setAttribute('target', '_black')
+            linkEl.setAttribute('href', repo.html_url)
+
+            let listItemEl = document.createElement('li')
+            listItemEl.appendChild(imgEl)
+            listItemEl.appendChild(titleEl)
+            listItemEl.appendChild(descriptionEl)
+            listItemEl.appendChild(linkEl)
+
+            this.listEl.appendChild(listItemEl)
+        })
+    }
+}
+new App()
